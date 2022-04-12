2c60cd2bef3dcd55d273cfb72a8f7dcbd3946344
Lauren  H <98533788+laurenstiefel@users.noreply.github.com>
2022-04-12 17:40:57 +0000

    part a complete

---
+++ StringUtility.py
@@ -0,0 +1,17 @@
+class StringUtility:
+  def __init__(self,string):
+    self.word= string
+  def __str__(self):
+    return self.word
+  def vowels(self):
+    vowels ="aeiouAEIOU"
+    
+    for vowels in StringUtility:
+      if StringUnitility in vowels:
+        vowels=vowels+1
+      return vowels
+  #def bothEnds(self):
+  #def fixStart(self):
+  #def asciiSum(self):
+  #def cipher(self):
+  
